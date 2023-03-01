79942a8a4d9480d7825d3598cce1d4a1c8f912bc
xerox0000 <toore@Debian.home>
2023-03-01 18:32:02 +0100

    Creazione analizzatore
    Creato un analizzatore grafico che permette di scegliere la dimensione delle celle per monitorare le zone più frequentate. Su dimensioni grandi (max 100) ricrea la mappa esaminata con variazione di colori per le zone più frequentate,per dimensioni piccole genera una griglia con le frequenze ma non molto comprensibile

--- Script/xmlAnalyze.py
+++ Script/xmlAnalyze.py
@@ -1,33 +1,41 @@
 import xml.etree.ElementTree as ET
-from collections import Counter
 
-# Parse the XML file
-tree = ET.parse('dati.xml')
+# Leggi il file XML
+tree = ET.parse('/home/toore/Tirocinio_SUMO/Scenario/Modena/fcd_dump.xml')
 root = tree.getroot()
 
-# Create a dictionary to count the number of occurrences of each position
-counter = Counter((int(child.attrib['x']), int(child.attrib['y'])) for child in root)
+# Crea un dizionario per le informazioni sui veicoli/persone
+veicoli = {}
 
-# Determine the maximum x and y coordinates
-max_x = max(int(child.attrib['x']) for child in root)
-max_y = max(int(child.attrib['y']) for child in root)
+# Definisci la griglia quadrata di celle con dimensioni fisse
+dim_cella = 10 # dimensione della cella in metri
+x_min, y_min, x_max, y_max = (0, 0, 1000, 1000) # coordinate della mappa
+celle = []
+for x in range(x_min, x_max, dim_cella):
+    for y in range(y_min, y_max, dim_cella):
+        celle.append(Cell(x, y))
 
-# Create a two-dimensional list to represent the grid
-grid = [[0 for y in range(max_y + 1)] for x in range(max_x + 1)]
+# Analizza i dati per ogni veicolo/persona
+for veicolo in root.findall('.//veicolo'):
+    id_veicolo = veicolo.get('id')
+    celle_visit = [] # lista di celle visitate
+    last_cella = None # ultima cella visitata
+    num_cambi_cella = 0 # numero di cambi di cella
+    for pos in veicolo.findall('./posizione'):
+        x = float(pos.get('x'))
+        y = float(pos.get('y'))
+        cella = get_cella(x, y, celle) # ottieni la cella corrispondente
+        if cella is not None:
+            if cella != last_cella:
+                num_cambi_cella += 1
+                last_cella = cella
+            if cella not in celle_visit:
+                celle_visit.append(cella)
+    veicoli[id_veicolo] = [celle_visit, num_cambi_cella]
 
-# Populate the grid with the counts from the dictionary
-for position, count in counter.items():
-    x, y = position
-    grid[x][y] = count
-
-# Determine the most frequently visited zones
-most_frequent = []
-for x in range(max_x + 1):
-    for y in range(max_y + 1):
-        if grid[x][y] == max(grid[x]):
-            most_frequent.append((x, y))
-
-# Print the most frequent zones
-print('Most frequent zones:')
-for zone in most_frequent:
-    print(zone)
+# Funzione per ottenere la cella corrispondente alle coordinate (x, y)
+def get_cella(x, y, celle):
+    for cella in celle:
+        if cella.x <= x < cella.x + dim_cella and cella.y <= y < cella.y + dim_cella:
+            return cella
+    return None
---
+++ Script/xmlAnalyzer_graphic.py
@@ -0,0 +1,52 @@
+import xml.etree.ElementTree as ET
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Definisci gli argomenti da linea di comando
+parser = argparse.ArgumentParser()
+parser.add_argument('file', type=str, help='nome del file XML contenente le coordinate')
+parser.add_argument('--n', type=int, default=10, help='dimensione della griglia (default: 10)')
+args = parser.parse_args()
+
+# Leggi il file XML e ottieni le coordinate x e y
+tree = ET.parse(args.file)
+root = tree.getroot()
+x_coords = [float(coord.get('x')) for coord in root.findall('.//person')]
+y_coords = [float(coord.get('y')) for coord in root.findall('.//person')]
+
+# Calcola la larghezza e l'altezza di ogni cella
+width = (max(x_coords) - min(x_coords)) / args.n
+height = (max(y_coords) - min(y_coords)) / args.n
+
+# Crea una matrice vuota per rappresentare la griglia
+grid = [[0 for _ in range(args.n)] for _ in range(args.n)]
+
+# Aggiungi ogni punto alle celle corrispondenti nella griglia
+for i, j in zip(x_coords, y_coords):
+    row = int((j - min(y_coords)) // height)
+    col = int((i - min(x_coords)) // width)
+    if row >= args.n or col >= args.n:
+        continue
+    grid[row][col] += 1
+
+# Stampa la griglia
+# for row in grid:
+#     for cell in row:
+#         print(cell, end=' ')
+#     print()
+
+# Crea una visualizzazione grafica della griglia
+fig, ax = plt.subplots()
+im = ax.imshow(grid, cmap='inferno')
+
+# Aggiungi i numeri di conteggio come testo nelle celle della griglia
+for i in range(args.n):
+    for j in range(args.n):
+        text = ax.text(j, i, grid[i][j], ha='center', va='center', color='white')
+
+# Aggiungi una barra di colori per la visualizzazione
+cbar = ax.figure.colorbar(im, ax=ax)
+
+# Mostra la visualizzazione grafica
+plt.show()
