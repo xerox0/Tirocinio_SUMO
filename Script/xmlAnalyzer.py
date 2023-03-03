c850185ce0b60cac98d27986e8f9c8429f3bb956
xerox0000 <toore@Debian.home>
2023-03-03 19:00:24 +0100

    Correzione codice
    Correzione del codice precedente perchè non logicamente giusto

--- Script/xmlAnalyzer.py
+++ Script/xmlAnalyzer.py
@@ -1,38 +1,42 @@
 import xml.etree.ElementTree as ET
 import argparse
 # Funzione per leggere il file XML e creare la lista di persone/veicoli
-def leggi_persone_da_xml(file_name):
-    tree = ET.parse(file_name)
+import xml.etree.ElementTree as ET
+
+
+def leggi_dati_xml(file_xml,dimensione_cella):
+    tree = ET.parse(file_xml)
     root = tree.getroot()
 
-    persone = {}
+    # dizionario per tenere traccia delle celle visitate per ogni persona/veicolo
+    celle_visit_person = {}
 
     for person in root.findall('.//person'):
-        id = str(person.get('id'))
-        x = float(person.get('x'))
-        y = float(person.get('y'))
-        timestamp = root.get('timestamp')
-        pos = (x, y)
+        id_person = str(person.attrib['id'])
+        x = float(person.attrib['x'])
+        y = float(person.attrib['y'])
 
-        # Aggiorna le informazioni sulla persona/veicolo corrente
-        if id not in persone:
-            persone[id] = {'celle_visit': set(), 'cambi_cella': 0}
-        celle_visit = persone[id]['celle_visit']
-        cambi_cella = persone[id]['cambi_cella']
+        # determina la cella corrente in base alle coordinate x e y
+        cella_x = int(x / dimensione_cella)
+        cella_y = int(y / dimensione_cella)
+        cella_corrente = (cella_x, cella_y)
 
-
-        # cella_x = int(x / dimensione_cella)
-        # cella_y = int(y / dimensione_cella)
-
-
-        if pos not in celle_visit:
-            celle_visit.add(pos)
+        if id_person not in celle_visit_person:
+            celle_visit_person[id_person] = {
+                'celle_visit': {cella_corrente},
+                'num_cambi_cella': 0
+            }
+        else:
+            celle_visit_person[id_person]['celle_visit'].add(cella_corrente)
+            # verifica se la cella corrente è diversa dall'ultima visitata
+            celle_visit = celle_visit_person[id_person]['celle_visit']
             if len(celle_visit) > 1:
-                cambi_cella += 1
-        persone[id]['cambi_cella'] = cambi_cella
-        persone[id]['celle_visit'] = celle_visit
+                ultima_cella = list(celle_visit)[-2]
+                num_cambi_cella = celle_visit_person[id_person]['num_cambi_cella']
+                if ultima_cella != cella_corrente:
+                    celle_visit_person[id_person]['num_cambi_cella'] = num_cambi_cella + 1
 
-    return persone
+    return celle_visit_person
 
 # Dimensione di ciascuna cella in metri
 dimensione_cella = 5.0
@@ -41,18 +45,19 @@ dimensione_cella = 5.0
 parser = argparse.ArgumentParser()
 parser.add_argument('file', type=str, help='nome del file XML contenente le coordinate')
 args = parser.parse_args()
-persone = leggi_persone_da_xml(args.file)
+dati = leggi_dati_xml(args.file,dimensione_cella)
 
 # Calcola le statistiche persona/veicolo
 fx = open("Dati.txt", "w")
-for id, info in persone.items():
+for id, info in dati.items():
     celle_visit = info['celle_visit']
-    cambi_cella = info['cambi_cella']
+    cambi_cella = info['num_cambi_cella']
 
     # Stampa le informazioni sulla persona/veicolo corrente
 
     print(f"ID: {id}",file=fx)
     print(f"Numero di celle visitate: {len(celle_visit)}",file=fx)
     print(f"Numero di cambi di cella: {cambi_cella}",file=fx)
+
     print(file=fx)
 fx.close()
