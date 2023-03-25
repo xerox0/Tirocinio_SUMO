fb0f2360088153e58d16e17dcf8326e02901ab9b
xerox0000 <toore@Debian.home>
2023-03-25 02:06:14 +0100

    Effettuate modifiche
    Rimosso codice xmlPerson per errori logici,rimpiazzato con xmlGridStats.py in cui è stata corretta la definizione della griglia e di ogni singola cella. Per comodità l'output è salvato in cell_stats.csv

--- Script/xmlGridstats.py
+++ Script/xmlGridstats.py
@@ -1,7 +1,10 @@
 import xml.etree.ElementTree as ET
 import math
 import statistics
+import pandas as pd
 import argparse
+
+
 
 
 GRID_SIZE = 10
@@ -12,9 +15,11 @@ MIN_Y, MAX_Y = -850, 850
 grid = {}
 for i in range(MIN_X, MAX_X, GRID_SIZE):
     for j in range(MIN_Y, MAX_Y, GRID_SIZE):
-        cell_x = i
-        cell_y = j
-        grid[(cell_x, cell_y)] = []
+        cell_x1 = i
+        cell_x2 = i + GRID_SIZE
+        cell_y1 = j
+        cell_y2 = j + GRID_SIZE
+        grid[(cell_x1, cell_y1, cell_x2, cell_y2)] = []
 
 
 parser = argparse.ArgumentParser()
@@ -24,18 +29,22 @@ tree = ET.parse(args.file)
 root = tree.getroot()
 
 for person in root.findall('.//person'):
+    person_id = person.get('id')
     x, y = float(person.get('x')), float(person.get('y'))
-    cell_x = GRID_SIZE * (math.floor(x/GRID_SIZE))
-    cell_y = GRID_SIZE * (math.floor(y/GRID_SIZE))
-    cell = (cell_x, cell_y)
-    grid[cell].append((x, y))
+    cell_x1 = GRID_SIZE * (math.floor(x/GRID_SIZE))
+    cell_y1 = GRID_SIZE * (math.floor(y/GRID_SIZE))
+    cell_x2 = cell_x1 + GRID_SIZE
+    cell_y2 = cell_y1 + GRID_SIZE
+    cell = (cell_x1, cell_y1, cell_x2, cell_y2)
+    if person_id not in [p[0] for p in grid[cell]]:
+        grid[cell].append((person_id, x, y))
 
-# Funzione per calcolare le statistiche di ogni cella
+
 def get_cell_stats(grid):
     cell_stats = {}
     for cell, people in grid.items():
-        x_coords = [p[0] for p in people]
-        y_coords = [p[1] for p in people]
+        x_coords = [p[1] for p in people]
+        y_coords = [p[2] for p in people]
         cell_stats[cell] = {
             'num_people': len(people),
             'std_dev_x': statistics.stdev(x_coords) if len(x_coords) > 1 else 0,
@@ -43,14 +52,8 @@ def get_cell_stats(grid):
         }
     return cell_stats
 
-# Esempio di utilizzo
 cell_stats = get_cell_stats(grid)
-fx=open("Variazioni_celle.txt","w")
-for cell, stats in cell_stats.items():
-    print(f'Cella {cell}:',file=fx)
-    print(f'Numero di persone: {stats["num_people"]}',file=fx)
-    print(f'Deviazione standard x: {stats["std_dev_x"]}',file=fx)
-    print(f'Deviazione standard y: {stats["std_dev_y"]}',file=fx)
-    print(file=fx)
 
-fx.close()
+results = pd.DataFrame(cell_stats.items(), columns=['cell', 'stats'])
+results = pd.concat([results.drop(['stats'], axis=1), results['stats'].apply(pd.Series)], axis=1)
+results.to_csv('cell_stats.csv', index=False)
